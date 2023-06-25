0b712c249808088b0867e1f6b7db8fe18a4eec4e
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-06-25 15:40:05 -0300

    Set the animation for navbar collapse hide

---
+++ src/scripts/collapseAnimation.js
@@ -0,0 +1,4 @@
+document.getElementById("navbarSupportedContent").addEventListener("mouseleave", function() {
+    var navbarCollapse = new bootstrap.Collapse(this);
+    navbarCollapse.hide();
+  });
